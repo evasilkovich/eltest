ab66ca79774bbe8797fdc7f18dab62c44b801f17
evasilkovich <evasilkovich@perfsys.net>
2021-04-24 15:30:34 +0300

    buildspec

--- run-test.sh
+++ run-test.sh
@@ -3,16 +3,19 @@
 #This shell script updates Postman environment file with the API Gateway URL created
 # via the api gateway deployment
 
-echo "Running update-postman-env-file.sh"
+echo "Running run-test.sh"
+
+#api_gateway_url=`aws cloudformation describe-stacks \
+#  --stack-name eltest-dev \
+#  --query "Stacks[0].Outputs[*].{OutputValueValue:OutputValue}" --output text`
+#
+#echo "API Gateway URL:" ${api_gateway_url}
+
 
 api_gateway_url=`aws cloudformation describe-stacks \
   --stack-name eltest-dev \
-  --query "Stacks[0].Outputs[*].{OutputValueValue:OutputValue}" --output text`
-
-echo "API Gateway URL:" ${api_gateway_url}
-
-api_gateway_url1=`aws cloudformation describe-stacks \
-  --stack-name eltest-dev \
   --query "Stacks[0].Outputs[? OutputKey == 'ServiceEndpoint'].OutputValue" --output text`
 
-echo "API Gateway URL 1:" ${api_gateway_url1}
+echo "API Gateway URL 1:" ${api_gateway_url}
+export API_GATEWAY_URL=api_gateway_url
+python ./test/hello.py
