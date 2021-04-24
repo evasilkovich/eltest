b0fc4ea169b09f8d08a47cedc3dd72218e34f395
evasilkovich <evasilkovich@perfsys.net>
2021-04-24 16:46:39 +0300

    buildspec

--- run-test.sh
+++ run-test.sh
@@ -12,12 +12,12 @@ echo "Running run-test.sh"
 #echo "API Gateway URL:" ${api_gateway_url}
 
 
-api_gateway_url=`aws cloudformation describe-stacks \
+export API_GATEWAY_URL=`aws cloudformation describe-stacks \
   --stack-name eltest-dev \
   --query "Stacks[0].Outputs[? OutputKey == 'ServiceEndpoint'].OutputValue" --output text`
 
-echo "API Gateway URL 1:" ${api_gateway_url}
-export API_GATEWAY_URL=api_gateway_url
+echo "API Gateway URL 1:" ${API_GATEWAY_URL}
+#export API_GATEWAY_URL=api_gateway_url
 ls -l
 
 cd test
