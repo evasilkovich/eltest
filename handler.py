7822699ae865d4a91d5b49bff4238c7647f1379f
evasilkovich <evasilkovich@perfsys.net>
2021-04-26 18:04:20 +0300

    put object

--- handler.py
+++ handler.py
@@ -1,8 +1,24 @@
 import boto3
+import json
+import os
+import time
+
+def put_object():
+    s3 = boto3.client('s3')
+    bucket_name = os.environ.get('MY_BUCKET')
+    json_object = {'datetime': time.strftime("%Y-%m-%d %H:%M:%S")}
+    key_name = 'data/'+ time.strftime("%Y-%m-%d-%H:%M:%S")+'.json'
+    s3.put_object(
+        Body=str(json.dumps(json_object)),
+        Bucket=bucket_name,
+        Key=key_name
+    )
+    return 0
+
 
 def hello(event, context):
 
-
+    put_object()
     response = {
         "statusCode": 200,
         "body": "buyworld"
