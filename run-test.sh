5e09c32652ef37e51b6b2d3f14c404df904187ed
evasilkovich <evasilkovich@perfsys.net>
2021-04-26 08:14:01 +0300

    run test in separated stack

--- run-test.sh
+++ run-test.sh
@@ -4,7 +4,7 @@ echo "Running run-test.sh"
 
 
 export API_GATEWAY_URL=`aws cloudformation describe-stacks \
-  --stack-name eltest-dev \
+  --stack-name eltest-test \
   --query "Stacks[0].Outputs[? OutputKey == 'ServiceEndpoint'].OutputValue" --output text`
 
 echo "API Gateway URL 1:" ${API_GATEWAY_URL}
@@ -13,3 +13,4 @@ ls -l
 cd test
 ls -l
 python hello.py
+cd ..
